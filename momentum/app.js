ed89b7e9591497e0552535e785c95cf3162ca5a1
LeeBG <donny1848@gmail.com>
2023-09-29 23:33:03 +0900

    JS momentum 공부

---
+++ momentum/app.js
@@ -0,0 +1,9 @@
+// 페이지의 제목을 가져오는 코드로
+console.log(document.title);
+
+// 페이지의 제목을 바꾸는 것도 가능하다;.
+document.title = "HI";
+
+// title은 document객체의 한 속성일 뿐이다.
+
+// 우리가 HTML에서 여러 정보를 가져올 수 있고 바꿀 수도 있다.
